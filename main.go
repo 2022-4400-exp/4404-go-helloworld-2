2a8a8a42b29fe72c8f860bb6141b5b0e68a56dcb
sonicexe <m19008@g.metro-cit.ac.jp>
2023-01-10 15:15:24 +0900

    argo cd

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "net/http"
+    "github.com/labstack/echo"
+)
+
+func main() {
+    e := echo.New()
+
+    e.GET("/hello", Hello())
+    e.GET("/api/hello", ApiHelloGet())
+
+    e.Start(":8080")
+}
+
+func Hello() echo.HandlerFunc {
+    return func(c echo.Context) error {
+        return c.String(http.StatusOK, "4404: You'er doomed.")
+    }
+}
+
+func ApiHelloGet() echo.HandlerFunc {
+    return func(c echo.Context) error {
+        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4404", "message": "You'er doomed."})
+    }
+}
