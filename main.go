b08297dfb52c1cfe73cffab14b1144f27fe9a585
sonicexe <m19008@g.metro-cit.ac.jp>
2023-01-10 15:25:42 +0900

    main.go chainged

--- main.go
+++ main.go
@@ -16,12 +16,12 @@ func main() {
 
 func Hello() echo.HandlerFunc {
     return func(c echo.Context) error {
-        return c.String(http.StatusOK, "4404: You'er doomed.")
+        return c.String(http.StatusOK, "4404: You are doomed.")
     }
 }
 
 func ApiHelloGet() echo.HandlerFunc {
     return func(c echo.Context) error {
-        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4404", "message": "You'er doomed."})
+        return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4404", "message": "You are doomed."})
     }
 }
